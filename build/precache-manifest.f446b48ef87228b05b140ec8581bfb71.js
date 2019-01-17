32422350344d8bf94e7c489ffaa62cab3396bbd7
Ruslan Chernov <chernruslan@gmail.com>
2019-01-17 17:39:19 +0300

    Build react-app

---
+++ build/precache-manifest.f446b48ef87228b05b140ec8581bfb71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe4dd54699b99b7561eb",
+    "url": "/static/js/main.fe4dd546.chunk.js"
+  },
+  {
+    "revision": "fad10d37a3502b446731",
+    "url": "/static/js/1.fad10d37.chunk.js"
+  },
+  {
+    "revision": "fe4dd54699b99b7561eb",
+    "url": "/static/css/main.583d656f.chunk.css"
+  },
+  {
+    "revision": "eb7746720597d6c0effa914c0132b80e",
+    "url": "/index.html"
+  }
+];
